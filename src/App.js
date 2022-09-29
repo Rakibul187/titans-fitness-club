65976129dcfb4053936331b671aff2c5d5150cf6
Rakibul Islam <rakibul241221@gmail.com>
2022-09-29 12:30:25 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Blog from './components/Blog/Blog';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Main></Main>
       <Blog></Blog>
+      <Footer></Footer>
     </div>
   );
 }
