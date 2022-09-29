9a460efbcdc2bdeb964fb4b50bb28c727a447963
Rakibul Islam <rakibul241221@gmail.com>
2022-09-29 11:59:59 +0600

    data load & show on ui

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -2,7 +2,13 @@ import React from 'react';
 import "./Workout.css"
 const Workout = ({ exercise }) => {
     return (
-       
+        <div className='exercise-item-cart'>
+            <img src={exercise.img} alt="" />
+            <h2>{exercise.category}</h2>
+            <p>{90 > exercise.benifit.length ? exercise.benifit.slice(0, 90) : exercise.benifit}</p>
+            <h5>Time: {exercise.time}</h5>
+            <button type='button'>Add To List</button>
+        </div>
     );
 };
 
